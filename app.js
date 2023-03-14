9eeb3a05cc0a20fab47e3c3c5d015e8426e49ffb
Andrian Aji <andrian@Andrians-MacBook-Air.local>
2023-03-14 18:34:29 +0700

    deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = require('./routers/index.js');
 const cors = require('cors');
-const port = process.env.PORT || 7543;
+const port = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
@@ -15,5 +15,33 @@ app.use(router);
 // app.listen(port, () => {
 //   console.log(`App is listening to ${port}`);
 // });
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: 'containers-us-west-165.railway.app',
+  port: 4001,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'TemanMain-user',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+// Test the connection by logging the Sequelize version
+(async () => {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+  console.log(`Sequelize version: ${sequelize.version}`);
+})();
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
 
 module.exports = app;
