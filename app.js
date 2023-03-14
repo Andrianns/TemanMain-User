42c8036e2f2b258da0dc9a6685c61c1d784d6694
Andrian Aji <andrian@Andrians-MacBook-Air.local>
2023-03-14 15:57:14 +0700

    node modules

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const router = require('./routers/index.js');
 const cors = require('cors');
-const port = 4003;
+const port = process.env.PORT || 4003;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
+
 app.listen(port, () => {
   console.log(`App is listening to ${port}`);
 });
